e41f0676bc944f917632ba820e3135e58a2f74ef
dean <d.lewis.rave@gmail.com>
2021-10-20 14:33:58 +0100

    10- Docking station releases bikes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,7 +2,6 @@ require 'bike'
 describe Bike do
   it 'check bike is working' do
     bike = Bike.new
-    expect(bike).respond_to?(:working?).with(1).argument
+    expect(bike).respond_to? :working?
   end
- 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,7 @@ describe DockingStation do
     expect(docking_station).respond_to? :release_bike
   end
   it 'gets bike' do
-    bike = Bike.new
-    expect(bike).to :release_bike
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to respond_to :working?
   end
 end
